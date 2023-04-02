e267ae46f64e02de63ee031bca542ad7acf52b73
SatoShinya2024 <satoshinya20812@gmail.com>
2023-04-02 14:24:32 +0900

    都道府県のチェックボックスをグリッドレイアウトで表示可能に。この後、グラフのサンプルを作成していく。

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 
-function Checkbox(prop: { prefCode: number; prefName: string }) {
+function Checkbox(prop: { Code: number; Name: string }) {
+  const checkbox_style = {
+    margin:"0 5px"
+  }
+  const label_style = {
+    height:"100%",
+  }
+
+  let name = "prefecture" + prop.Code;
+
   return (
     <div>
-      <input type="checkbox" value={prop.prefCode} />
-      <span>{prop.prefName}</span>
+      <input type="checkbox" value={prop.Code} style={checkbox_style} name={name} id={name}/>
+      <label style={label_style} htmlFor={name}>{prop.Name}</label>
     </div>
   );
 }
--- src/Prefecture.tsx
+++ src/Prefecture.tsx
@@ -1,43 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import { JsxElement } from "typescript";
 import Checkbox from "./Checkbox";
 
 function Prefecture() {
-  let pref_array: { prefCode: number; prefName: string }[] = [
-    { prefCode: 0, prefName: "問題が発生しました。一度ページを離れて入りなおしてください。" },
-  ];
+  let pref_array: {
+    message: string;
+    result: { prefCode: number; prefName: string }[];
+  } = { message: "", result: [] };
   const [prefectures, setPrefectures] = useState(pref_array);
-
-  //ステートにAPIから取得した都道府県一覧のオブジェクトを格納する関数
-  const getPrefecture = async () => {
-    //リクエスト
-    const resp = await fetch(
-      "https://opendata.resas-portal.go.jp/api/v1/prefectures",
-      {
+  const getPrefecture_data = useMemo(
+    () =>
+      fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
         method: "GET",
         headers: {
           "X-API-KEY": "x6Cenztodyveqvc5b4RlSf0Dvzwgv5sTK2jgyo05",
         },
-      }
-    );
-    //APIから取得したデータをJSON形式に変換
-    const result: {
-      message: string;
-      result: { prefCode: number; prefName: string }[];
-    } = await resp.json();
+      })
+        .then((res) => res.json())
+        .then((res) => setPrefectures(res))
+        .then((res) => console.log("do")),
+    []
+  );
 
-    console.log(result);
-    //取得したデータをステートに格納する
-    setPrefectures(result.result);
-  };
-  getPrefecture();
+  const prefectures_style = {
+    margin:"0 auto",
+    width:"80%",
+    display: "grid",
+    'grid-template-columns':"17% 17% 17% 17% 17%",
+    'grid-template-rows':'7% 7% 7% 7% 7% 7% 7% 7% 7% 7%',
+    gap:"3%",
+  }
+
   return (
-    <div>
-      {pref_array.map((value) => {
+    <div style={prefectures_style}>
+      {prefectures.result.map((value) => {
+        console.log("do");
         return (
           <Checkbox
-            prefCode={value.prefCode}
-            prefName={value.prefName}
+            Code={value.prefCode}
+            Name={value.prefName}
             key={value.prefCode}
           />
         );
